6764ec15dca0c74a56639371eb76a57eb33bf164
Ivan Login <ivan.login.02@mail.ru>
2023-03-28 01:33:27 +0300

    Добавил решение задач 10,13,15

---
+++ Sem2Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите 3 значное число");
+
+string? numStr = Console.ReadLine();
+
+if(numStr != null)
+{
+    int numInt = int.Parse(numStr);
+
+    Console.WriteLine((numInt/10)%10);  
+}
---
+++ Sem2Task13/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+// * Сделать вариант для числа длиной до 10 цифр не используя char или string
+
+Console.WriteLine("Введите число");
+
+//Обычный вариант
+
+// string? NumStr = Console.ReadLine();
+
+// if (NumStr != null)
+// {
+//     char[] dig = NumStr.ToString().ToCharArray();
+
+//     if(dig.Length > 2)
+//     {
+//     Console.WriteLine((int)(dig[2])-48);
+//     }
+//     else
+//     {
+//     Console.WriteLine("Третьей цифры нет");
+//     }
+// }
+
+
+// Вариант со *
+int? NumStr = Convert.ToInt32(Console.ReadLine());
+
+//ToInt32 - Преобразует заданное значение в 32-битовое целое число (не в 16 битовое как short). 
+
+if (NumStr > 0)
+{
+    if (NumStr > 99)
+    {
+        while (NumStr > 999)
+        {
+            NumStr = NumStr / 10;
+        }
+        Console.WriteLine(NumStr % 10);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+}
